6c9a2025dc360aaa00205265a90019b8067b2d88
Umutari <umutariy@gmail.com>
2017-09-15 16:34:31 +0000

    sms app

--- SMSApp.php
+++ SMSApp.php
@@ -4,17 +4,17 @@
 	<title>PTA</title>
 </head>
 <body>
+	<div>
+		<form action="" method="">
+			<label>SendeID (your name)</label><br>
+			<input type="" name=""><br>
+			<label>Message</label><br>
+			<textarea name="message"; style="width:300px; height:100px;""></textarea><br>
+			<label>Phone numbers</label>
+			<input type="" name="">
+		</form>
+	</div>
 
-	<form action="" method="">
-		<label>SendeID (your name)</label><br>
-		<input type="" name=""><br>
-		<label>Message</label><br>
-		<textarea name="message"; style="width:300px; height:100px;""></textarea><br>
-		<label>Phone numbers</label>
-		<input type="" name="">
-
-		
-	</form>
 
 </body>
 </html>
