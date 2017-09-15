3e2d01e84913ee41ad59f317b89f69bd57e65adb
Joseph Mills <mills.josef@gmail.com>
2017-09-15 13:55:55 +0000

    screenshot added. Http request set up

--- main.php
+++ main.php
@@ -1,5 +1,6 @@
 <?php
-    $sender = $phone = $message = "";
+    //intialise variables
+    $sender = $phone = $message = $response = "";
 
     if (isset($_POST["sender"]) && $_POST["phone"] && $_POST["message"]){
         $sender = $_POST["sender"];
@@ -9,6 +10,19 @@
 
     echo "Hello $sender. Number: $phone. Message: $message";
     
-    $url = ""
-
+    $url = "144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to=$phone&from=$sender&text=$message&dlr-mask=31";
+    
+    //echo $url;
+    //create new HttpRequest object
+    $req = new HttpRequest($url, HttpRequest::METH_POST);
+    
+    try{
+        $req.send();
+        if($req.getResponseCode() = 200){
+            $response = $req.getRawResponseMessage();
+            echo $response;
+        }
+    }catch(HttpException $exc){
+        echo $exc;
+    }
 ?>
