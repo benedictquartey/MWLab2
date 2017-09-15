09711a6125e80db55949c0b117415162b7ff53b7
Umutari <umutariy@gmail.com>
2017-09-15 16:24:18 +0000

    my sms app

---
+++ SMSApp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PTA</title>
+</head>
+<body>
+
+	<form action="" method="">
+		<label>SendeID (your name)</label><br>
+		<input type="" name=""><br>
+		<label>Message</label><br>
+		<textarea name="message"; style="width:300px; height:100px;""></textarea><br>
+		<label>Phone numbers</label>
+		<input type="" name="">
+
+		
+	</form>
+
+</body>
+</html>
