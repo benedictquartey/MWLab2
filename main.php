7898eda1e22bee1121e6770dcc865d7ebe99b48b
Joseph Mills <mills.josef@gmail.com>
2017-09-15 13:34:49 +0000

    data received in php

--- main.php
+++ main.php
@@ -1,6 +1,14 @@
 <?php
+    $sender = $phone = $message = "";
+
+    if (isset($_POST["sender"]) && $_POST["phone"] && $_POST["message"]){
+        $sender = $_POST["sender"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+    }
+
+    echo "Hello $sender. Number: $phone. Message: $message";
     
-
-
+    $url = ""
 
 ?>
