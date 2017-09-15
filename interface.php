b7b4cf65e9f0ca3cbffaae993ec10a745e37a470
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-09-15 21:42:27 +0100

    my app

--- interface.php
+++ interface.php
@@ -12,17 +12,23 @@
   
       <?php
 	    if(!empty(isset($_POST["submit"]))){
-		getMessage();	
+		$msg = '';
+		$recipient1 = '';
+		$recipient2 = '';
+		$recipient3 = '';
+		sendMessage();	
 		}
 			
-	    //the function to validate the form data 
-		function getMessage(){
-			
-			//validating the message 
-			if((($_POST["message"]) !="") and (isset($_POST["message"]))){
-						$comments = $_POST["comments"];
-					}
+	    //the function to send the message
+		function sendMessage(){
+		    $msg = $_POST['message'];
+			$recipient1 =$_POST['r1'];
+			$recipient2 =$_POST['r2'];
+			$recipient3 =$_POST['r3'];
 
+            header("location: http://144.76.58.179:16243/cgi-bin/sendsms?
+			username=senduser&password=sendpass&to=".$recipient1."+".$recipient2."+".$recipient3."&from=Bhebhe&text=".$msg."&dlr-mask=31"
+			}
 	     ?>
 		
 		
@@ -34,7 +40,7 @@
 		  <div id= icon>
 		  <img src="smsImage.jpg"  height="100" width="100">
 		  </div>
-		  <div>
+		  <div id=header>
 		  <h1> St Andrews Announcements </h1> <br> 
 		  </div>
 		  Recipient 1: <input type="text" name="r1"> <br> <br>
