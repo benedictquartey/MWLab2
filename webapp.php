56694eb4e12b19f5e7c336a1aa1ee8e3ab1ac30f
Lynn Mumia <lynn.mumia@ashesi.edu.gh>
2017-09-15 21:10:57 +0000

    Final commit

--- webapp.php
+++ webapp.php
@@ -10,34 +10,32 @@
 		   <img class ="img" src="logo.png">
 		      <h2 class="Title">Lynn Jimmy's School</h2>
 		      <!--The form that has the imput variables-->
-		     	<form class="form">
+		     	<form class="form" method="GET" action="">
 			     	 SenderId:
 		  			 <input type="text" name="senderid" id="senderid"><br><br>
 
 			     	 Phone Numbers:
-		 			 <input type="text" name="number" id="number" placeholder="separate with '+' sign"><br><br>
+		 			 <input type="text" name="number" id="number" placeholder="separate number with commas"><br><br>
 
 		  			 Message:
 		  			<textarea rows="4" cols="50" type="text" name="message" id ="message"></textarea><br> <br>             
-		     		<button type="send" class="btn btn-primary" onclick="sendsms()">Send</button>
+		     		<button type="send" name ="send" class="btn btn-primary">Send</button>
 					  
 				</form>
-         <!--			<script>
-                  /*The function to send the sms*/
-
-				  function sendsms(){
-					   var senderid= document.getElementByid("senderid");
-					   var phonenumber= document.getElementByid("number");				   
-					   var message= document.getElementByid("message");
-					   alert(senderid);
-					   var url = "144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to="+$phonenumber+"&from=" + $senderid + "&text="+ $message +"&dlr-mask=31";	
-					   window.open(url);
-                  }
 
 
+				<?php
+					if(isset($_GET["send"])){
 
+						echo "working";
 
-				</script>	
-				-->     
+						$senderid=$_GET["senderid"];
+						$phonenumbers= explode(",",$_GET["number"]);
+						$message= $_GET["message"];
+						header("location: http://144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to=".$phonenumbers[1]."+".$phonenumbers[2]."+".$phonenumbers[3]."&from=".$senderid."&text=".$message."&dlr-mask=31");
+				   }  
+			   
+
+                ?>
 		</body>
 	</html>
