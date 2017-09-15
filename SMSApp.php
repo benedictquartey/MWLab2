a70eb7b7c6666f3e20a79c23f6bf643ae3ae29cd
Umutari <umutariy@gmail.com>
2017-09-15 16:41:36 +0000

    made some changes

--- SMSApp.php
+++ SMSApp.php
@@ -1,17 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>PTA</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
-	<div>
-		<form action="" method="">
-			<label>SendeID (your name)</label><br>
-			<input type="" name=""><br>
-			<label>Message</label><br>
-			<textarea name="message"; style="width:300px; height:100px;""></textarea><br>
-			<label>Phone numbers</label>
-			<input type="" name="">
+	<div class="container">
+		<form action="" method="post">
+			<fieldset>
+				<label>SendeID (your name)</label><br>
+				<input type="" name=""><br>
+			</fieldset>
+			<fieldset>
+				<label>Phone numbers</label>
+				<input type="" name="">
+			</fieldset>
+			<fieldset>
+				<label>Message</label><br>
+				<textarea name="message"; style="width:300px; height:100px;""></textarea><br>
+			</fieldset>
+			<fieldset>
+				<button name="login" type="submit" id="loginSubmit" data-submit="...Sending">Submit</button>
+				<a href="../register/index.php">Sign Up</a>
+			</fieldset>
+			
 		</form>
 	</div>
 
