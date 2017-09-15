c6610c285aa8bb9f362d5c77a24a5665de36704e
warihanagumah <warihana.gumah@ashesi.edu.gh>
2017-09-15 20:19:05 +0000

    This my Final commit

--- index.php
+++ index.php
@@ -8,23 +8,22 @@
 
 	<body>
 
-    <?php include_once("processIndex.php");
-    ?>
+    <?php include_once("processIndex.php");?>
       
 		<div class="container">  
-  			<form id="contact" action="" method="post">
-    			<h3>Send New SMS</h3>
-   
-    			<fieldset>
-      				<input placeholder="Enter Receipient(s)" type="text" name="contact" tabindex="1" required autofocus><br><br>
-    			</fieldset>
-    			<fieldset>
-      				<textarea placeholder="Type your Message Here...." name ="message" tabindex="5" required></textarea><br><br>
-    			</fieldset>
-    			<fieldset>
-      				<button name="send" type="submit" id="contact-submit" data-submit="...Sending">Send</button>
-    			</fieldset>
-    		</form>	
+  		<form id="contact" action="" method="post">
+    		<h3>Send New SMS</h3>   
+    		<fieldset>
+      		<input placeholder="Enter Receipient(s)" type="text" name="contact" tabindex="1" required autofocus><br><br>
+    		</fieldset>
+    		<fieldset>
+      		<textarea placeholder="Type your Message Here...." name ="message" tabindex="5" required></textarea><br><br>
+    		</fieldset>
+    		<fieldset>
+      		<button name="send" type="submit" id="contact-submit" data-submit="...Sending">Send</button>
+    		</fieldset>
+    	</form>	
+    </div>    
 	</body>
 
 </html>
