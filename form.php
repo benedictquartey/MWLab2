0cfa86549d37c0e87b24042d1b46a89f0a013328
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2017-09-16 16:39:56 +0000

    Updated the files

---
+++ form.php
@@ -0,0 +1,36 @@
+<?php
+$action=$_REQUEST['action'];
+    /* The contact form UI */
+    ?>
+    <form   method="POST">
+    <input type="hidden" name="action" value="submit">
+    Receipient1:<br>
+    <input name="R1" type="text" size="15"/><br>
+    Receipient1:<br>
+    <input name="R2" type="text" size="15"/><br>
+    Receipient1:<br>
+    <input name="R3" type="text" size="15"/><br>
+    Text Message:<br>
+    <textarea name="sms" rows="7" cols="30"></textarea><br>
+    <a href="<?php /*This variable caries the link as below*/echo $link_address;?>"> Send </a>
+    </form>
+    <?php 
+               /* Variables entered by the user*/
+    
+    $No1=$_REQUEST['R1'];
+    $No2=$_REQUEST['R2'];
+    $No3=$_REQUEST['R3'];
+    $Message=$_REQUEST['sms'];
+    if (($No1=="")||($No2=="")||($No3=="")||($Message==""))
+        {
+		echo "Please fill all the films";
+	    }else{
+            /*Creating the url with the right variables*/
+            $recepients=$No1."+".$No2."+".$No3;
+            $SenderID="lesapiti";
+            $link_address="144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to=".$recepients."&from=".$SenderID."&text=".$Message."&dlr-mask=31";
+            
+        }
+
+?>
+						
