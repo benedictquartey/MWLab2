db34831b8725f464c61913fb6ad371b192a0a636
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2017-09-17 12:46:08 +0000

    Updated

--- form.php
+++ form.php
@@ -6,13 +6,13 @@ $action=$_REQUEST['action'];
     <input type="hidden" name="action" value="submit">
     Receipient1:<br>
     <input name="R1" type="text" size="15"/><br>
-    Receipient1:<br>
+    Receipient2:<br>
     <input name="R2" type="text" size="15"/><br>
-    Receipient1:<br>
+    Receipient3:<br>
     <input name="R3" type="text" size="15"/><br>
     Text Message:<br>
     <textarea name="sms" rows="7" cols="30"></textarea><br>
-    <a href="<?php /*This variable caries the link as below*/echo $link_address;?>"> Send </a>
+    <a href="<?php echo $link_address;?>"> Send </a>
     </form>
     <?php 
                /* Variables entered by the user*/
@@ -23,14 +23,13 @@ $action=$_REQUEST['action'];
     $Message=$_REQUEST['sms'];
     if (($No1=="")||($No2=="")||($No3=="")||($Message==""))
         {
-		echo "Please fill all the films";
-	    }else{
+        echo "Please fill all the films";
+        }else{
             /*Creating the url with the right variables*/
             $recepients=$No1."+".$No2."+".$No3;
-            $SenderID="lesapiti";
-            $link_address="144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to=".$recepients."&from=".$SenderID."&text=".$Message."&dlr-mask=31";
+                    $link_address="http://144.76.58.179:16243/cgi-bin/sendsms?username=senduser&password=sendpass&to=".$recepients."&from=lesapiti&text=".$Message."&dlr-mask=31";
             
         }
 
 ?>
-						
+                        
